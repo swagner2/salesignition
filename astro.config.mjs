2302c8b43bf6623da1b81d610498d899c6944c9f
Steven Wagner <steven@sales-ignition.com>
2026-08-14 15:13:42 -0400

    feat: consolidate RetentionOS carrd site as /retention-os and set up path redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough'
   }),
-  integrations: [react(), markdoc(), keystatic()]
+  integrations: [react(), markdoc()],
+  redirects: {
+    '/index.html': '/',
+    '/retentionos': '/retention-os'
+  }
 });
