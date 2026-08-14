c7adffd69e38b630bb6981bd5a6127184f0cc331
Steven Wagner <steven@sales-ignition.com>
2026-08-14 14:38:30 -0400

    feat: enable keystatic integration in astro config to resolve blank page error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
     imageService: 'passthrough'
   }),
-  integrations: [react(), markdoc()],
-  redirects: {
-    '/index.html': '/'
-  }
+  integrations: [react(), markdoc(), keystatic()]
 });
